61e1837892035892a35e2e8f4c9014ab214de656
Alex Marochko <marochko@gmail.com>
2025-07-31 22:26:49 +0300

    feat(ui): add MosaicCloud as background element for cv_web_page.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_cv/core/theme/cubit/theme_cubit.dart';
 import 'package:flutter_cv/features/cv/presentation/cubit/cv_cubit.dart';
 import 'package:flutter_cv/features/cv/presentation/cubit/cv_state.dart';
 import 'package:flutter_cv/features/cv/presentation/pages/responsive_cv_page.dart';
-import 'package:flutter_cv/features/loading/ui/loading_screen.dart';
+
 import 'core/di/service_locator.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
+import 'features/loading/presentation/loading_screen.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -20,31 +21,40 @@ class CvApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return BlocProvider(
-        create: (_) => ThemeCubit(),
+      create: (_) => ThemeCubit(),
       child: Builder(
         builder: (context) {
           return BlocBuilder<ThemeCubit, ThemeMode>(
             builder: (context, themeMode) {
               return MaterialApp(
                 title: 'Flutter CV',
-                theme:  themeMode == ThemeMode.light? ThemeData.light() : ThemeData.dark(),
+                theme:
+                    themeMode == ThemeMode.light
+                        ? ThemeData.light()
+                        : ThemeData.dark(),
                 home: BlocProvider(
                   create: (_) => CvCubit(sl())..loadCv(),
                   child: BlocBuilder<CvCubit, CvState>(
                     builder: (context, state) {
                       return AnimatedSwitcher(
                         duration: const Duration(milliseconds: 500),
-                        child: (state is CvLoaded)
-                            ? ResponsiveCvPage(key: const ValueKey('cv_page'), cv: state.cv)
-                            : const LoadingScreen(key: ValueKey('loading_screen')),
+                        child:
+                            (state is CvLoaded)
+                                ? ResponsiveCvPage(
+                                  key: const ValueKey('cv_page'),
+                                  cv: state.cv,
+                                )
+                                : const LoadingScreen(
+                                  key: ValueKey('loading_screen'),
+                                ),
                       );
                     },
                   ),
                 ),
               );
-            }
+            },
           );
-        }
+        },
       ),
     );
   }
