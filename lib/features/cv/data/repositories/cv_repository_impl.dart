9185df934028857e55d21362d60122207d4ce61a
Alex Marochko <marochko@gmail.com>
2025-08-02 09:54:48 +0300

    fix: error handling

--- lib/features/cv/data/repositories/cv_repository_impl.dart
+++ lib/features/cv/data/repositories/cv_repository_impl.dart
@@ -1,3 +1,5 @@
+import 'package:flutter_cv/core/error/exceptions.dart';
+import 'package:flutter_cv/core/error/failures.dart';
 import 'package:flutter_cv/features/cv/data/datasources/cv_data_source.dart';
 import 'package:flutter_cv/features/cv/domain/entities/cv.dart';
 import 'package:flutter_cv/features/cv/domain/repositories/cv_repository.dart';
@@ -9,8 +11,13 @@ class CvRepositoryImpl implements CvRepository {
 
   @override
   Future<Cv> getCv() async {
-    final model = await dataSource.fetchRawData();
-
-    return model.toEntity();
+    try {
+      final model = await dataSource.fetchRawData();
+      return model.toEntity();
+    } on ServerException {
+      throw GeneralFailure(message: 'Failed to load data from server');
+    } catch (e) {
+      throw GeneralFailure(message: e.toString());
+    }
   }
 }
