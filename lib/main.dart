37a86ad65eb2bf6a73eb85d7c7b465ca7e4b9d39
Alex Marochko <marochko@gmail.com>
2025-08-03 13:49:03 +0300

    feat: add Firebase to the project

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,6 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_crashlytics/firebase_crashlytics.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_cv/core/di/service_locator.dart';
@@ -10,6 +13,17 @@ import 'package:flutter_cv/features/loading/presentation/loading_screen.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+
+  FlutterError.onError = (errorDetails) {
+    FirebaseCrashlytics.instance.recordFlutterFatalError(errorDetails);
+  };
+  // Pass all uncaught asynchronous errors that aren't handled by the Flutter framework to Crashlytics
+  PlatformDispatcher.instance.onError = (error, stack) {
+    FirebaseCrashlytics.instance.recordError(error, stack, fatal: true);
+    return true;
+  };
+
   await init(); // DI setup
 
   runApp(const CvApp());
@@ -40,17 +54,17 @@ class CvApp extends StatelessWidget {
                         duration: const Duration(milliseconds: 500),
                         child: switch (state) {
                           CvLoaded() => ResponsiveCvPage(
-                              key: const ValueKey('cv_page'),
-                              cv: state.cv,
-                            ),
+                            key: const ValueKey('cv_page'),
+                            cv: state.cv,
+                          ),
                           CvError() => ErrorScreen(
-                              key: const ValueKey('error_screen'),
-                              failure: state.failure,
-                              onRetry: () => context.read<CvCubit>().loadCv(),
-                            ),
+                            key: const ValueKey('error_screen'),
+                            failure: state.failure,
+                            onRetry: () => context.read<CvCubit>().loadCv(),
+                          ),
                           _ => const LoadingScreen(
-                              key: ValueKey('loading_screen'),
-                            ),
+                            key: ValueKey('loading_screen'),
+                          ),
                         },
                       );
                     },
