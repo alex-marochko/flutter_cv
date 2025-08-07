0454d629f3dd1dee7d1243b0dda4b2d0762a18e4
Alex Marochko <marochko@gmail.com>
2025-08-07 19:50:36 +0300

    chore: remove old test helper

--- test/helpers/test_helpers.dart
+++ test/helpers/test_helpers.dart
@@ -1,56 +1,7 @@
-import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_cv/core/services/crash_reporting/crash_reporting_service.dart';
-import 'package:flutter_test/flutter_test.dart';
 import 'package:mocktail/mocktail.dart';
 
 class MockCrashReportingService extends Mock implements CrashReportingService {}
 
 typedef Callback = void Function(MethodCall call);
-
-void setupFirebaseCoreMocks() {
-  TestWidgetsFlutterBinding.ensureInitialized();
-  // Mock Firebase.initializeApp()
-  setupFirebaseCoreMocksForTest();
-}
-
-Future<void> setupFirebaseCoreMocksForTest({Callback? customCallback}) async {
-  final original = Firebase.delegatePackingProperty;
-  TestWidgetsFlutterBinding.ensureInitialized();
-
-  // Mock the FirebaseCore platform channel
-  final channel = MethodChannel('plugins.flutter.io/firebase_core');
-  TestDefaultBinaryMessengerBinding.instance.defaultBinaryMessenger
-      .setMockMethodCallHandler(channel, (call) async {
-        if (call.method == 'Firebase#initializeCore') {
-          return [
-            {
-              'name': defaultFirebaseAppName,
-              'options': {
-                'apiKey': '123',
-                'appId': '1:123:android:123',
-                'messagingSenderId': '123',
-                'projectId': '123',
-              },
-              'pluginConstants': {},
-            },
-          ];
-        }
-        if (call.method == 'Firebase#initializeApp') {
-          return {
-            'name': call.arguments['appName'],
-            'options': call.arguments['options'],
-            'pluginConstants': {},
-          };
-        }
-
-        if (customCallback != null) {
-          customCallback(call);
-        }
-
-        return null;
-      });
-
-  // Restore the original delegate after the test is complete
-  addTearDown(() => Firebase.delegatePackingProperty = original);
-}
